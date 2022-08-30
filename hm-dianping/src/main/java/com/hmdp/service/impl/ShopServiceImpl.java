a8dca5d097cd4ac7b38e2351e442632f641dd8eb
redyouzi <1589029587@qq.com>
2022-08-30 11:54:58 +0800

    互斥锁解决缓存穿击

--- hm-dianping/src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,5 +1,6 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.BooleanUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
 import com.hmdp.dto.Result;
@@ -35,6 +36,22 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
 
     @Override
     public Result queryById(Long id) {
+        //缓存穿透
+//        Shop shop = queryWithPassThrough(id);
+        //缓存击穿 互斥锁
+        Shop shop = queryWithMutex(id);
+        if (shop==null){
+            return Result.fail("店铺不存在");
+        }
+        return Result.ok(shop);
+    }
+
+    /**
+     * 互斥锁 解决缓存穿击
+     * @param id
+     * @return
+     */
+    public Shop queryWithMutex(Long id){
         String key = RedisConstants.CACHE_SHOP_KEY + id;
         //1.从redis中查询商铺缓存
         String shopJSON = stringRedisTemplate.opsForValue().get(key);
@@ -42,12 +59,90 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         if (StrUtil.isNotBlank(shopJSON)) {
             //3.存在,返回
             Shop shop = JSONUtil.toBean(shopJSON, Shop.class);
-            return Result.ok(shop);
+            return shop;
         }
         //判断是否为空字符串  ->   防止缓存穿透
 //        if (shopJSON!=null){ //经过上面的if判断过滤后,剩下的只有 null 或者 "" 或者 不可见字符
         if ("".equals(shopJSON)){
-            return Result.fail("查询的商铺不存在");
+            return null;
+        }
+
+        //TODO 开始实现缓存重建
+        String lockKey= "lock:shop:"+id;
+        Shop shop;
+        try {
+            //1.获取互斥锁
+            boolean isLock = tryLock(lockKey);
+            //2.判断是否获取成功
+            if (!isLock){
+                //获取锁失败,休眠,并重试
+                Thread.sleep(50);
+                return queryWithMutex(id);
+            }
+
+            //region Description DoubleCheck
+            //TODO 再次检查redis缓存是否存在 DoubleCheck
+            // 防止第一个线程释放锁之后第二个线程立马获取到了锁(第二个线程前面的redis缓存已经检查过了发现没有缓存,但是此时第一个线程已经重建了缓存)
+            // 防止重复更新缓存
+            //1.从redis中查询商铺缓存
+            String doubleCheckShopJSON = stringRedisTemplate.opsForValue().get(key);
+            //2.判断是否存在商铺信息
+            if (StrUtil.isNotBlank(doubleCheckShopJSON)) {
+                //3.存在,返回
+                return JSONUtil.toBean(doubleCheckShopJSON, Shop.class);
+            }
+            //判断是否为空字符串  ->   防止缓存穿透
+//        if (shopJSON!=null){ //经过上面的if判断过滤后,剩下的只有 null 或者 "" 或者 不可见字符
+            if ("".equals(doubleCheckShopJSON)){
+                return null;
+            }
+            //endregion
+
+            //TODO 获取锁成功 重建缓存数据
+            //4.如果成功,根据id查询数据库
+            //根据id查询数据库
+            shop = this.getById(id);
+            //5.数据库中也不存在,返回错误
+            if (shop==null){
+                //将空字符串写入到缓存中,并设置2分钟的过期时间
+                stringRedisTemplate.opsForValue().set(key,"",SHOP_NULL_TTL,TimeUnit.MINUTES);
+
+                return null;
+            }
+            //6.数据库中存在,写入到redis中,并设置超时时间
+            String jsonStr = JSONUtil.toJsonStr(shop);
+            stringRedisTemplate.opsForValue().set(key,jsonStr,SHOP_ID_TTL, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            //TODO 释放互斥锁
+            unLock(lockKey);
+
+        }
+        // 返回
+        return shop;
+
+    }
+
+    /**
+     * 缓存穿透
+     * @param id
+     * @return
+     */
+    public Shop queryWithPassThrough(Long id){
+        String key = RedisConstants.CACHE_SHOP_KEY + id;
+        //1.从redis中查询商铺缓存
+        String shopJSON = stringRedisTemplate.opsForValue().get(key);
+        //2.判断是否存在商铺信息
+        if (StrUtil.isNotBlank(shopJSON)) {
+            //3.存在,返回
+            Shop shop = JSONUtil.toBean(shopJSON, Shop.class);
+            return shop;
+        }
+        //判断是否为空字符串  ->   防止缓存穿透
+//        if (shopJSON!=null){ //经过上面的if判断过滤后,剩下的只有 null 或者 "" 或者 不可见字符
+        if ("".equals(shopJSON)){
+            return null;
         }
         //4.不存在 ,根据id查询数据库
         Shop shop = this.getById(id);
@@ -56,12 +151,30 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
             //将空字符串写入到缓存中,并设置2分钟的过期时间
             stringRedisTemplate.opsForValue().set(key,"",SHOP_NULL_TTL,TimeUnit.MINUTES);
 
-            return Result.fail("店铺不存在");
+            return null;
         }
         //6.数据库中存在,写入到redis中,并设置超时时间
         String jsonStr = JSONUtil.toJsonStr(shop);
         stringRedisTemplate.opsForValue().set(key,jsonStr,SHOP_ID_TTL, TimeUnit.MINUTES);
-        return Result.ok(shop);
+        return shop;
+    }
+
+    /**
+     * 添加锁 如果不存在
+     * @param key 锁名
+     * @return boolean
+     */
+    private boolean tryLock(String key){
+        Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(key, "1", 10, TimeUnit.SECONDS);
+        return BooleanUtil.isTrue(flag);
+    }
+
+    /**
+     * 释放锁
+     * @param key
+     */
+    private void unLock(String key){
+        stringRedisTemplate.delete(key);
     }
 
     /**
